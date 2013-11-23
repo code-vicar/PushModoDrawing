6b7e089b00d6366d2e02458dd93aaef7377a05a3
Scott Vickers <scott.w.vickers@gmail.com>
2013-11-22 16:59:31 -0800

    point oauth at pushmodo.com

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,7 +12,7 @@ private
 
   def api_auth
     config = PushModoDrawing::Application.config
-    client = OAuth2::Client.new(config.pushmodo[:id], config.pushmodo[:secret], :site => "http://localhost:3000")
+    client = OAuth2::Client.new(config.pushmodo[:id], config.pushmodo[:secret], :site => "http://pushmodo.com")
     @api = client.password.get_token(config.pushmodo[:email], config.pushmodo[:password])
     
   end
